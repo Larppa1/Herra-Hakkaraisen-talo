1c4d1b8858bc1c3c598c11fcd3641aa2a64931ca
Antti Hokkinen <antti.hokkinen@tuni.fi>
2022-01-16 15:53:18 +0200

    Added colored to colorless switch

--- Assets/Scripts/Countdown.cs
+++ Assets/Scripts/Countdown.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Countdown : MonoBehaviour {
     public Text countdowntext;
     private float count = 20;
+    public GameObject varillinen;
+    public GameObject variton;
 
     // Update is called once per frame
     void Update(){
         if(count>0){
             count -= Time.deltaTime;
             countdowntext.text = ((int)count).ToString();
+        }
+        else if ((int)count == 0){
+            varillinen.SetActive(false); 
+            variton.SetActive(true);
+            varillinen.GetComponent<Renderer>().enabled = false;
+            variton.GetComponent<Renderer>().enabled = true;
         }
     }
 }
