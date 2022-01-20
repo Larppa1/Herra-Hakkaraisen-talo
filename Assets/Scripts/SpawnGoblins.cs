1ab2c7aea30340ed6a3f04b3c7fe5fd76565185f
Lari Juva <lari.juva7@gmail.com>
2022-01-20 15:16:03 +0200

    Added SpawnGoblins script

---
+++ Assets/Scripts/SpawnGoblins.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGoblins : MonoBehaviour
+{
+    public GameObject obstacle;
+    public float maxX;
+    public float minX;
+    public float maxY;
+    public float minY;
+    public float timeBetweenSpawn;
+    private float spawnTime;
+
+    void Update()
+    {
+        if (Time.time > spawnTime)
+        {   
+            float randomX = Random.Range(minX, maxX);
+            float randomY = Random.Range(minY, maxY);
+            Instantiate(obstacle, transform.position = new Vector2(randomX, randomY), transform.rotation);
+            spawnTime = Time.time + timeBetweenSpawn;
+        }
+    }
+}
