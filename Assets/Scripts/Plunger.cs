0d688271aa1fc48c2ecdc291106fc2b8d4db1760
Lari Juva <lari.juva7@gmail.com>
2022-01-26 15:53:12 +0200

    Minor changes to plunger animations

--- Assets/Scripts/Plunger.cs
+++ Assets/Scripts/Plunger.cs
@@ -8,8 +8,11 @@ namespace TIKO4A2021
     public class Plunger : MonoBehaviour{
         private Vector2 plungerDirection;
         public float plungerSpeed;
+        public GameObject enemy;
         private Rigidbody2D plungerBody;
-        private bool isMoved = false;
+        private int count = 0;
+        private bool isRemoved = true;
+
         void Start(){
             plungerBody = GetComponent<Rigidbody2D>();
         }
@@ -19,20 +22,29 @@ namespace TIKO4A2021
             PlungerProperties.position = transform.position;
             
             if(Input.GetKeyDown("down")) {
-                transform.DOMove(new Vector2(transform.position.x, -3), 1).SetLoops(1, LoopType.Yoyo);
-                isMoved = true;
+                transform.DOMove(new Vector2(transform.position.x, -3), 1).SetLoops(2, LoopType.Yoyo);
             }
 
-            if(isMoved && PlungerProperties.isCaught) {
-                transform.DOMove(new Vector2(transform.position.x, 10), 1).SetLoops(1, LoopType.Yoyo);
-                isMoved = false;
-            }else if(isMoved) {
-                transform.DOMove(new Vector2(transform.position.x, 3), 1).SetLoops(1, LoopType.Yoyo);
-                isMoved = false;
+            if(transform.position.y > 3 && PlungerProperties.isCaught) {
+                if(count == 0) {
+                    transform.DOMove(new Vector2(transform.position.x, 10), 1).SetLoops(2, LoopType.Yoyo);
+                    isRemoved = false;
+                }
+                count++;
             }
         }
         void FixedUpdate(){
             plungerBody.velocity = new Vector2(plungerDirection.x * plungerSpeed, 0);
+        }
+
+        private void OnTriggerStay2D(Collider2D collision) {
+            if(collision.tag == "Goblin") {
+                if(transform.position.y > 8 && isRemoved == false) {
+                    //Destroy enemy
+                    //isRemoved = true;
+                    //PlungerProperties.isCaught = false;
+                }
+            }
         }
     }
 }
