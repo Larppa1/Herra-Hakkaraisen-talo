084cd2823aa3b9d7f442c12d5e6b541270f2a3f5
Lari Juva <lari.juva7@gmail.com>
2022-02-10 12:42:13 +0200

    Terrain generator added

---
+++ Assets/Scripts/Autopiha/TerrainController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+namespace TIKO4A2021 {
+    public class TerrainController : MonoBehaviour {
+        public SpriteShapeController shape;
+        public int scale;
+
+        void Start() {
+            shape = GetComponent<SpriteShapeController>();
+            int amountOfPoints = scale / 6;
+            float distanceBetweenPoints = (float)scale / (float)amountOfPoints;
+
+            shape.spline.SetPosition(2, shape.spline.GetPosition(2) + Vector3.right * scale);
+            shape.spline.SetPosition(3, shape.spline.GetPosition(3) + Vector3.right * scale);
+
+            for(int i = 0; i < amountOfPoints; i++) {
+                float xPos = shape.spline.GetPosition(i + 1).x + distanceBetweenPoints;
+                shape.spline.InsertPointAt(i + 2, new Vector2(xPos, 18 * Mathf.PerlinNoise(i * Random.Range(5.0f, 25.0f), 0)));
+            }
+
+            for(int i = 2; i < amountOfPoints + 2; i++) {
+                shape.spline.SetTangentMode(i, ShapeTangentMode.Continuous);
+                shape.spline.SetLeftTangent(i, new Vector2(-1.8f, 0));
+                shape.spline.SetRightTangent(i, new Vector2(1.8f, 0));
+            }
+        }
+    }
+}
