9a3e9148130892ae2e1b18a7e99ed1fa9d1a492c
Antti Hokkinen <antti.hokkinen@tuni.fi>
2022-02-20 19:48:23 +0200

    AP Fuelbar made usable in any scale

--- Assets/Scripts/Autopiha/CoinCollector.cs
+++ Assets/Scripts/Autopiha/CoinCollector.cs
@@ -10,12 +10,12 @@ namespace TIKO4A2021 {
                 case "Coin": CoinManager.amount += 5; break;
                 case "Coin2": CoinManager.amount += 10; break;
                 case "Coin3": CoinManager.amount += 15; break;
-                case "JerryCan": if(FuelBar.change > 0.6f){
-                        FuelBar.change = 1f;
-                    }else{
-                        FuelBar.change += 0.4f;
-                    }break;
-                case "GasStation": FuelBar.change = 1f; break;
+                case "JerryCan": 
+                    FuelBar.current += 40;
+                    break;
+                case "GasStation": 
+                    FuelBar.current = 100; 
+                    break;
             }
         }
     }
--- Assets/Scripts/Autopiha/FuelBar.cs
+++ Assets/Scripts/Autopiha/FuelBar.cs
@@ -1,24 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace TIKO4A2021{
     public class FuelBar : MonoBehaviour{
         //Gameoverpanel
         public static bool isGameOver = false;
-
-        public static float change = 1;
+        public static float timer;
+        public static float current;
+        private int max = 100;
+        public float timerSize;
+        public Image mask;
+        //public int offset;
 
         // Update is called once per frame
+        void Awake(){
+            current=100;
+        }
         void Update(){
-            if(transform.localScale.x > 0){
-                change -= 0.00001f;
-                transform.localScale = new Vector2(change, 1);
+            if(current > 100){
+                current =100;
+            }else if(current < 0){
+                current = 0;
             }
-            else{
+            if(current > 0){
+                current -= 1/timerSize;
+            }else{
                 //gameOverPanel.setActive(true);
                 isGameOver = true;
             }
+            GetCurrentFill();
+        }
+
+        void GetCurrentFill(){
+            float fillAmount = (float)current / (float)max;
+            mask.fillAmount = fillAmount;
         }
 
     }
