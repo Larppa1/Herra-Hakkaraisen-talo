2509c5aea9db14522c7c1ad377bf318a9a6dbd09
Lari Juva <lari.juva7@gmail.com>
2022-04-18 21:57:37 +0300

    GH pictures

--- Assets/Scripts/Galleriahuone/GalleryDifficulty.cs
+++ Assets/Scripts/Galleriahuone/GalleryDifficulty.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 namespace TIKO4A2021{
     public class GalleryDifficulty : MonoBehaviour{
-        public GameObject gameManager, difficultyPanel, canvas;
+        public GameObject gameManager, difficultyPanel, canvas, easyReal, normalReal, hardReal;
+        public GameObject[] easyPictures = new GameObject[5];
+        public GameObject[] normalPictures = new GameObject[5];
+        public GameObject[] hardPictures = new GameObject[5];
         public static float countSize;
         public void GalleryDifficultyLevel(string difficultyLevel){
             switch(difficultyLevel){
                 case "easy":
                     countSize = 20;
+                    
                     break;
                 case "normal":
                     countSize = 15;
+                    
                     break;
                 case "hard":
                     countSize = 10;
+                    
                     break;
             }
             gameManager.SetActive(true);
             difficultyPanel.SetActive(false);
             canvas.SetActive(true);
         }
-
     }
 }
