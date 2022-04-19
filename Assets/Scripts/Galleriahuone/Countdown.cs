39ed3a01a8eff531142681b142d9f0c956585bc1
Lari Juva <lari.juva7@gmail.com>
2022-04-19 10:48:55 +0300

    GH stuff

--- Assets/Scripts/Galleriahuone/Countdown.cs
+++ Assets/Scripts/Galleriahuone/Countdown.cs
@@ -6,22 +6,41 @@ using UnityEngine.UI;
 namespace TIKO4A2021 {
     public class Countdown : MonoBehaviour {
         public Text countdowntext;
-        public GameObject topLeft, topRight, bottomLeft, bottomRight, readyButton;
-        private SpriteRenderer topLeftRenderer, topRightRenderer, bottomLeftRenderer, bottomRightRenderer;
+        public GameObject readyButton, easyReal, normalReal, hardReal;
+        public GameObject[] easyPictures = new GameObject[5];
+        public GameObject[] normalPictures = new GameObject[5];
+        public GameObject[] hardPictures = new GameObject[5];
         public static float coloringTimer = 0;
         void Update() {
             if(GalleryDifficulty.countSize>0){
                 GalleryDifficulty.countSize -= Time.deltaTime;
                 countdowntext.text = ((int)GalleryDifficulty.countSize).ToString();
             }else if ((int)GalleryDifficulty.countSize == 0) {
-                topLeft.GetComponent<SpriteRenderer>().color = Color.white;
-                topRight.GetComponent<SpriteRenderer>().color = Color.white;
-                bottomLeft.GetComponent<SpriteRenderer>().color = Color.white;
-                bottomRight.GetComponent<SpriteRenderer>().color = Color.white;
+                switch(GalleryDifficulty.difficulty) {
+                    case "easy":
+                        for(int i = 0; i < easyPictures.Length; i++) {
+                            easyPictures[i].SetActive(true);
+                            easyReal.SetActive(false);
+                        }
+                        break;
+                    case "normal":
+                        for(int i = 0; i < normalPictures.Length; i++) {
+                            normalPictures[i].SetActive(true);
+                            normalReal.SetActive(false);
+                        }
+                        break;
+                    case "hard":
+                        for(int i = 0; i < hardPictures.Length; i++) {
+                            hardPictures[i].SetActive(true);
+                            hardReal.SetActive(false);
+                        }
+                        break;
+                }
 
                 ColorChecker.counted = true;
                 GalleryDifficulty.countSize--;
                 readyButton.SetActive(true);
+                countdowntext.gameObject.SetActive(false);
 
             }else if((int)GalleryDifficulty.countSize == -1) {
                 coloringTimer += Time.deltaTime;
--- Assets/Scripts/Galleriahuone/GalleryDifficulty.cs
+++ Assets/Scripts/Galleriahuone/GalleryDifficulty.cs
@@ -5,25 +5,24 @@ using UnityEngine;
 namespace TIKO4A2021{
     public class GalleryDifficulty : MonoBehaviour{
         public GameObject gameManager, difficultyPanel, canvas, easyReal, normalReal, hardReal;
-        public GameObject[] easyPictures = new GameObject[5];
-        public GameObject[] normalPictures = new GameObject[5];
-        public GameObject[] hardPictures = new GameObject[5];
         public static float countSize;
+        public static string difficulty;
         public void GalleryDifficultyLevel(string difficultyLevel){
             switch(difficultyLevel){
                 case "easy":
                     countSize = 20;
-                    
+                    easyReal.SetActive(true);
                     break;
                 case "normal":
                     countSize = 15;
-                    
+                    normalReal.SetActive(true);
                     break;
                 case "hard":
                     countSize = 10;
-                    
+                    hardReal.SetActive(true);
                     break;
             }
+            difficulty = difficultyLevel;
             gameManager.SetActive(true);
             difficultyPanel.SetActive(false);
             canvas.SetActive(true);
